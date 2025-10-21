0d37d46bc87597b360ca4a542e404b52dff14ab9
fernycrack <fernandojoseurquijorios@gmail.com>
2025-10-21 02:58:52 -0600

    Update formulario.js

--- HELPSIC/formulario.js
+++ HELPSIC/formulario.js
@@ -2,7 +2,7 @@ function calcularResultado() {
   let total = 0;
 
   // Sumar los valores seleccionados
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 21; i++) {
     const respuesta = document.querySelector(`input[name="p${i}"]:checked`);
     if (respuesta) {
       total += parseInt(respuesta.value);
@@ -13,15 +13,16 @@ function calcularResultado() {
   let mensaje = "";
   if (total === 0) {
     mensaje = "Responde todas las preguntas.";
-  } else if (total <= 25) {
-    mensaje = "Muy bien";
-  } else if (total >= 26 && total <= 29) {
-    mensaje = "Normal";
-  } else if (total > 29 && total <= 32) {
-    mensaje = "Necesita apoyo";
+  } else if (total <= 9) {
+    mensaje = "Muy bien, necesidad minima";
+  } else if (total >= 10 && total <= 16) {
+    mensaje = "Normal, necesidad leve";
+  } else if (total > 16 && total <= 29) {
+    mensaje = "Necesita apoyo, necesidad moderada";
   } else {
     mensaje = "Acercate, te ayudamos";
   }
 
   document.getElementById("resultado").textContent = `Puntos: ${total} - ${mensaje}`;
 }
+
